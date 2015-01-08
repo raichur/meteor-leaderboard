ee67c74dc85fa368cabff4214216db5a46ae7342
Joshua Raichur <hello@joshuaraichur.com>
2015-01-08 16:06:12 +0530

    add and remove players in the leaderboard

--- leaderboard.js
+++ leaderboard.js
@@ -1,6 +1,7 @@
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient){
+
   Template.leaderboard.helpers({
     player: function(){
       return PlayersList.find({}, {sort: {score:-1, name: 1}});
@@ -11,8 +12,13 @@ if(Meteor.isClient){
       if(playerId == selectedPlayer){
         return 'selected'
       }
+    },
+    'showSelectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer)
     }
   });
+
   Template.leaderboard.events({
     'click .player': function(){
       var playerId = this._id;
@@ -25,6 +31,21 @@ if(Meteor.isClient){
     'click .decrement': function(){
       var selectedPlayer = Session.get('selectedPlayer');
       PlayersList.update(selectedPlayer, {$inc: {score: -5}});
+    },
+    'click .remove': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.remove(selectedPlayer);
+    }
+  });
+
+  Template.addPlayerForm.events({
+    'submit form': function(event){
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0
+      });
     }
   });
 }
